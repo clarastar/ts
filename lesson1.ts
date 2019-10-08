f48200e74af1f31227b3f3bcac9397c14ffbc438
Clara <xiaohuiwang@natureself.cn>
2019-10-08 20:51:17 +0800

    first commit

---
+++ lesson1.ts
@@ -0,0 +1,27 @@
+
+function identity<T>(arg: T): T {
+  return arg
+}
+
+identity('string')
+identity(3)
+
+function identity1(arg: string): number {
+  return 3
+}
+
+identity1('number')
+
+function loggingIdentity<T>(arg: T[]): T[] {
+  console.log(arg.length)
+  return arg
+}
+
+function loggingIdentity2<T>(arg: Array<T>): Array<T> {
+  console.log(arg.length)
+  return arg
+}
+
+
+let myIdentity: <T>(arg: T) => T= identity
+let myIdentity1: <U>(arg: U) => U = identity
